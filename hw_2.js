12d4d858d1e1ed1e778862c5c00199be36641a87
Maksim Shkurdziuk <maxshkurduk@gmail.com>
2022-05-05 12:41:40 +0300

    Third update hw_2

--- hw_2.js
+++ hw_2.js
@@ -47,8 +47,15 @@
 // let b = 2;
 // let c = "белых медведей";
 
+// a = String(a);
+// b = String(b);
+
 // let twelveBears = a + b + " " + c;
 // console.log(twelveBears);
+
+// // Или
+
+// console.log(`${a}${b} ${c}`)
 
 // Task 5
 
