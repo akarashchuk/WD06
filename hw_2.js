b3c7dad5979e582e9ff8c4d22ffd38ac730c0376
Roman Parkachev <romanparkachev@gmail.com>
2022-05-08 11:24:15 +0300

    All last changes

---
+++ hw_2.js
@@ -0,0 +1,107 @@
+"use strict"
+
+// Task 1 
+
+let x = 20;
+let y = 58;
+let z = 42;
+
+console.log(x + y + z);
+
+
+// Task 2
+
+const SEC_IN_MIN = 60;
+const MIN_IN_HR = 60;
+const HR_IN_DAY = 24;
+const DAY_IN_YR = 365;
+let yearsNumber = 53; 
+const ageInSeconds = yearsNumber * DAY_IN_YR * HR_IN_DAY * MIN_IN_HR * SEC_IN_MIN
+
+console.log(ageInSeconds);
+
+
+// Task 3
+
+let count = 42; 
+let userName = '42'; 
+
+let count1 = String(count);
+console.log(typeof count1);
+
+let userName1 = +userName;
+console.log(typeof userName1);
+
+
+// Task 4
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+
+console.log(`${a}${b} ${c}`);
+
+
+// Task 5
+
+let d = "доступ"; 
+let m = "морпех";
+let n = "наледь";
+let p = "попрек";
+let r = "рубило";
+
+let lengthWords = (d + m + n + p + r).length;
+
+console.log(lengthWords);
+
+
+// Task 6
+
+let name = 'Jonh';
+let age = 25;
+let degree = 25 >= 18;
+
+let N = `Variable: Name's type: ${typeof name}`;
+let M = `Variable: Age's type: ${typeof age}`;
+let D = `Variable: Degree's type: ${typeof degree}`;
+
+console.log(N, M, D);
+
+
+
+// Task 7
+
+let yourName = prompt("Your name", "");
+let yourAge = prompt("Your age", "");
+console.log(yourName, yourAge);
+
+
+// Advanced Task 1
+
+let A = 4
+let B = 3
+
+A = A + B;
+B = A - B;
+A = A - B;
+
+// console.log(a, b);
+
+
+// Advanced Task 2
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+let first = codeWord1.charAt(1);
+let second = codeWord2.charAt(1);
+let third = codeWord3.charAt(1);
+let fourth = codeWord4.charAt(1);
+let fifth = codeWord5.charAt(1);
+
+let cipher = (first + second + third + fourth + fifth);
+
+console.log(cipher);
