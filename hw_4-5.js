9721e5a0af2e19a4e11843d324b1309c4de99e43
Tereschenko Tanya <trschnkt@gmail.com>
2022-05-18 21:31:42 +0300

    Updated hw_4-5

--- hw_4-5.js
+++ hw_4-5.js
@@ -11,12 +11,10 @@ function getSum(n) {
     for (let i = 1; i <= n; i++) {
         sum += i;
     }
-
     return sum;
 }
 
 console.log(getSum(100));
-
 
 // Task 2
 
@@ -27,7 +25,6 @@ let calcOverpay = amount => (amount * rate * years);
 
 console.log(calcOverpay(1000));
 
-
 // Task 3    ???
 
 function trimString(valueFrom, valueTo, str) {
@@ -35,12 +32,10 @@ function trimString(valueFrom, valueTo, str) {
     for (let str = valueFrom; str <= valueTo; str++) {
         console.log(str);
     }
-
     return str;
 }
 
 trimString(5, 10);
-
 
 // Task 4
 
@@ -52,13 +47,10 @@ function getSumNumbers(num) {
     for (let i = 0; i < numToStr.length; i++) {
         value += +(numToStr[i]);
     }
-
     return value;
-
 }
 
 console.log(getSumNumbers(2021));
-
 
 // Task 5    ???
 
@@ -73,12 +65,10 @@ function getSum(a, b) {
             sum += i;
         }
     }
-
     return sum;
 }
 
 console.log(getSum(2, 2));
-
 
 // Task 6    ???   
 
@@ -101,7 +91,6 @@ function boo() {
 
 fooboo(false);
 
-
 // Task 7
 
 let user = {
@@ -112,7 +101,6 @@ let user = {
 delete user.firstName;
 delete user.lastName;
 
-
 // Task 8
 
 let place = {
@@ -121,7 +109,6 @@ let place = {
 }
 
 console.log("city" in place);
-
 
 // Task 9
 
@@ -139,7 +126,6 @@ for (let key in student) {
     console.log(student[key]);
 }
 
-
 // Task 10
 
 const color = {
@@ -152,7 +138,6 @@ const color = {
 
 console.log(color['ru pum pu ru rum'].red);
 console.log(color['ru pum pu ru rum'].blue);
-
 
 //Task 11
 
@@ -174,8 +159,6 @@ for (let key in salaries) {
 let average = sum / Object.keys(salaries).length;
 
 console.log(average);
-
-
 
 // Task 12    ???
 
@@ -199,13 +182,11 @@ if (validation === 'да') {
     alert('Отменено');
 }
 
-
 // Task 13
 
 const colors = ['red', 'green', 'blue'];
 
 console.log(colors.length);
-
 
 // Task 14
 
@@ -228,7 +209,6 @@ console.log(numbers);
 numbers.length = 0;
 console.log(numbers);
 
-
 // Task 16
 
 const students = ['Polina', 'Dasha', 'Masha'];
@@ -239,7 +219,6 @@ students.shift();
 students.unshift('Andrey');
 
 console.log(students);
-
 
 // Task 17
 
@@ -253,20 +232,18 @@ for (let cat of cats) {
     console.log(cat);
 }
 
-
 // Task 18
 
-const evenNumbers = [2, 4, 6, 8, 10]
-const oddNumbers = [1, 3, 5, 7, 9]
+const evenNumbers = [2, 4, 6, 8, 10];
+const oddNumbers = [1, 3, 5, 7, 9];
 const allNumbers = evenNumbers.concat(oddNumbers);
 
 console.log(allNumbers);
 console.log(allNumbers.indexOf(8));
 
-
 // Task 19
 
-const arr = [0, 0, 0, 0]
+const arr = [0, 0, 0, 0];
 
 let str = arr.join('1');
 
@@ -279,7 +256,7 @@ console.log(str);
 
 function checkTriangle(a, b, c) {
 
-    if ((a + b > c) && (a + c > b) && (c + b > c)) {
+    if ((a + b > c) && (a + c > b) && (c + b > a)) {
         return true;
     } else {
         return false;
@@ -287,7 +264,6 @@ function checkTriangle(a, b, c) {
 }
 
 console.log(checkTriangle(3, 6, 5));
-
 
 // Task 2 
 
@@ -302,11 +278,10 @@ function checkPalindrom(palindrom) {
         } else {
             return false;
         }
-    };
+    }
 }
 
 console.log(checkPalindrom('заказ'));
-
 
 // Task 3  
 
@@ -325,10 +300,9 @@ let result = total / matrix.flat().length;
 
 console.log(result);
 
-
 // Task 4
 
-const mixedNumbers = [-14, 24, -89, 43, 0, -1, 412, 4]
+const mixedNumbers = [-14, 24, -89, 43, 0, -1, 412, 4];
 
 let negative = [];
 let positive = [];
