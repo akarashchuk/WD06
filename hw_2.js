4fa31d75ce2067fdd63e03b2fd431a2e53fc239a
Kirill Goroshko <gorosheck@gmail.com>
2022-05-24 10:45:15 +0300

    add hw_1.js, hw_2.md, hw_2.js

---
+++ hw_2.js
@@ -0,0 +1,87 @@
+"use strict"
+
+// Task 1
+
+let x = 20;
+let y = 58;
+let z = 42;
+let value = x + y + z;
+console.log(value);
+
+// Task 2
+
+let secondInMin = 60;
+let minInHour = 60;
+let hourInDay = 24;
+let dayInYear = 365;
+let myOld = 33;
+let valueTask2 = myOld * dayInYear * hourInDay * minInHour * secondInMin;
+console.log(valueTask2);
+
+// Task 3
+
+let count = 42;
+let userName = '42';
+let countChange = count;
+countChange = String(count);
+let userNameChange = userName;
+userNameChange = Number(userName);
+console.log(typeof count);
+console.log(typeof userName);
+console.log(typeof countChange);
+console.log(typeof userNameChange);
+
+// let count = 42 + '';
+// let userName = +'42';
+
+
+
+// Task 4
+
+let a = 1
+let b = 2
+let c = "белых медведей"
+let space = ' '
+a = String(a);
+b = String(b);
+let valueBear = a + b + space + c;
+console.log(valueBear);
+
+// Task 5
+
+let word1 = 'доступ';
+let word2 = 'морпех';
+let word3 = 'наледь';
+let word4 = 'попрек';
+let word5 = 'рубило';
+let lengthWords = word1 + word2 + word3 + word4 + word5;
+console.log(lengthWords.length);
+
+// Task 6
+
+let number = 11;
+let string = 'string';
+let bool = true;
+
+function showType(item) {
+   let type = typeof item;
+   console.log(`Variable: ${item} have type: ${type}`);
+}
+
+showType(number);
+showType(string);
+showType(bool);
+
+// Task 7
+
+let value666 = prompt("Введите Возраст", '');
+console.log(value666);
+
+// Task 8
+let a8 = 4;
+let b8 = 3;
+a8 -= 1;
+b8 += 1;
+
+// Task 9
+
