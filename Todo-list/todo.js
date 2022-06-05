9f43539d4dbac39e5501608cfe4c5fd51a4e133c
Roman Parkachev <romanparkachev@gmail.com>
2022-06-05 20:28:14 +0300

    Addind files in hw-todo

--- Todo-list/todo.js
+++ Todo-list/todo.js
@@ -1,18 +1,1 @@
 "use strict"
-
-const input = document.querySelector('#input');
-const btn = document.querySelector('#btn');
-const result = document.querySelector('#result');
-const total = document.querySelector('#total');
-
-btn.addEventListener('click', (e) => {
-   // result.innerHTML += `<li>${input.value}</li>`;
-   if (input.value === '') return
-   createDeleteElements(input.value)
-   input.value = ''
-})
-function createDeleteElements(value) {
-   console.log(value)
-
-   const li = document.createElement('li')
-}
