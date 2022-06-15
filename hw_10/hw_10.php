fe43d43dcedddd88b48b29552b9f3424ba55783d
Maksim Shkurdziuk <maxshkurduk@gmail.com>
2022-06-15 18:15:01 +0300

    init(hw_10): My 1st PHP homework

---
+++ hw_10/hw_10.php
@@ -0,0 +1,60 @@
+<?php
+
+//Task 1
+
+//$x = "Hello World";
+
+//Task 2
+
+//$x = 5;
+//$y = 3;
+//
+//echo $x + $y;
+
+//Task 3
+
+//echo mb_strlen('Привет мир!');
+
+// Task 4
+
+//$oldText = "Learn HTML!";
+//echo str_replace('HTML', 'PHP', $oldText);
+
+//Task 5
+
+//$x = 50;
+//$y = 10;
+//
+//if ($x > $y) {
+//    echo "x больше y";
+//} else {
+//    echo "x меньше y";
+//}
+
+//Task 6
+
+//$a = 20;
+//$b = 10;
+//
+//if ($a == $b) {
+//    echo "Первый";
+//} elseif ($a > $b) {
+//    echo "Второй";
+//} else {
+//    echo "Третий";
+//}
+
+//Task 7
+
+$color = 'red';
+
+switch ($color) {
+    case 'red':
+        echo "Hello";
+        break;
+    case 'green':
+        echo "Второй";
+        break;
+    default:
+        echo "Не тот цвет";
+}
