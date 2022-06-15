5d5966e78ea38e0192c0ea0060bd2d0c37f8327c
Vladislaw Samsonov <valsailen96@gmail.com>
2022-06-15 21:33:50 +0300

    hw10

---
+++ hw10/hw10.php
@@ -0,0 +1,63 @@
+<?php
+
+//Task 1
+//$x = 'Hello World';
+
+//Task 2
+
+//$x = 4;
+//$y = 8;
+//
+//echo $x + $y;
+
+
+//Task 3
+
+// $a = mb_strlen("Привет мир!");
+
+//echo $a;
+
+//Task 4
+
+//$oldText = "Learn HTML!";
+//
+//echo str_replace(search:'HTML', replace:'PHP', $oldText);
+
+
+////Task 5
+//
+//$x = 50;
+//$y = 10;
+//
+//if ($x > $y) {
+//    echo 'x больше y';
+//}
+
+//Task 6
+
+//$a = 20;
+//$b = 10;
+//
+//if ($a === $b) {
+//    echo 'Первый';
+//} elseif ($a > $b) {
+//    echo 'Второй';
+//} else {
+//    echo 'Третий';
+//}
+
+//Task 7
+
+$color = 'green';
+
+switch ($color) {
+    case  'red';
+        echo 'Hello' ;
+        break;
+    case  'green';
+        echo 'Второй' ;
+        break;
+    default:
+        echo 'попробуй еще';
+        break;
+}
