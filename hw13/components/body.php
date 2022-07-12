f2ab5d0b6dabb43fa239035107e013475107d131
Gonchar Nikita <nikitagonchar33@gmail.com>
2022-07-12 17:13:27 +0300

    hw 13 php

---
+++ hw13/components/body.php
@@ -0,0 +1,49 @@
+<!-- NAVIGATION -->
+<nav id="navigation">
+    <!-- container -->
+    <div class="container">
+        <!-- responsive-nav -->
+        <div id="responsive-nav">
+            <!-- NAV -->
+            <ul class="main-nav nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Hot Deals</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">Laptops</a></li>
+                <li><a href="#">Smartphones</a></li>
+                <li><a href="#">Cameras</a></li>
+                <li><a href="#">Accessories</a></li>
+            </ul>
+            <!-- /NAV -->
+        </div>
+        <!-- /responsive-nav -->
+    </div>
+    <!-- /container -->
+</nav>
+<!-- /NAVIGATION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- shop -->
+            <div class="col-md-4 col-xs-6">
+                <div class="shop">
+                    <div class="shop-img">
+                        <img src="./img/shop01.png" alt="">
+                    </div>
+                    <div class="shop-body">
+                        <h3>Laptop<br>Collection</h3>
+                        <a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <?php
+require __DIR__ . './blocks/collection.php';
+require __DIR__ . './blocks/deal.php';
+require __DIR__ . './blocks/newsletter.php';
+require __DIR__ . './blocks/products.php';
+require __DIR__ . './blocks/topselling.php';
+require __DIR__ . './blocks/topsellingleft.php';
+require __DIR__ . './blocks/topsellingcenter.php';
+require __DIR__ . './blocks/topsellingright';
