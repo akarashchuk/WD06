59ca7ec10418a36ca4f18948aa10913cdd3b2767
Tereschenko Tanya <trschnkt@gmail.com>
2022-07-17 22:22:52 +0300

    Added hw_13

---
+++ hw_13/components/blocks/main/new_products.php
@@ -0,0 +1,50 @@
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+
+            <!-- section title -->
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h3 class="title">New Products</h3>
+                    <div class="section-nav">
+                        <ul class="section-tab-nav tab-nav">
+                            <li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Smartphones</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Cameras</a></li>
+                            <li><a data-toggle="tab" href="#tab1">Accessories</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- /section title -->
+
+            <!-- Products tab & slick -->
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="products-tabs">
+                        <!-- tab -->
+                        <div id="tab1" class="tab-pane active">
+                            <div class="products-slick" data-nav="#slick-nav-1">
+                                <?php
+                                require __DIR__ . '/product_cards/new_product_1.php';
+                                require __DIR__ . '/product_cards/new_product_2.php';
+                                require __DIR__ . '/product_cards/new_product_3.php';
+                                require __DIR__ . '/product_cards/new_product_4.php';
+                                require __DIR__ . '/product_cards/new_product_5.php';
+                                ?>
+                            </div>
+                            <div id="slick-nav-1" class="products-slick-nav"></div>
+                        </div>
+                        <!-- /tab -->
+                    </div>
+                </div>
+            </div>
+            <!-- Products tab & slick -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+
---
+++ hw_13/components/footer.php
@@ -0,0 +1,12 @@
+<?php require __DIR__ . '/blocks/footer.php'; ?>
+
+<!-- jQuery Plugins -->
+<script src="/hw_13/electro/js/jquery.min.js"></script>
+<script src="/hw_13/electro/js/bootstrap.min.js"></script>
+<script src="/hw_13/electro/js/slick.min.js"></script>
+<script src="/hw_13/electro/js/nouislider.min.js"></script>
+<script src="/hw_13/electro/js/jquery.zoom.min.js"></script>
+<script src="/hw_13/electro/js/main.js"></script>
+
+</body>
+</html>
