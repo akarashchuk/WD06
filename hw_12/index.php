12c939e5d4dd164ca7bf0db9b18135090e084d8c
Mark Kaladzinsky <kristianomark7@gmail.com>
2022-07-19 00:50:43 +0300

    modified hw-12

--- hw_12/index.php
+++ hw_12/index.php
@@ -16,36 +16,32 @@ function sayHi(): string
 
 function getYear(int $year): bool
 {
-    if ($year % 4 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return $year % 4 === 0;
 }
 
 //Task 3
 
-function getAbbreviature(string $words): string
-{
-    $expr = '/(?<=\s|^)[A-Z]/';
-    preg_match_all($expr, $words, $matches);
-    $abbr = implode('', $matches[0]);
-    return $abbr;
-}
-
-print_r(getAbbreviature('Cascading Style Sheets'));
+//function getAbbreviature(string $words): string
+//{
+//    $expr = '/(?<=\s|^)[A-Z]/';
+//    preg_match_all($expr, $words, $matches);
+//    $abbr = implode('', $matches[0]);
+//    return $abbr;
+//}
+//
+//print_r(getAbbreviature('Cascading Style Sheets'));
 
 //Task 4
 
-function getReverse(string $words): string
-{
-    $arrWords = mb_str_split($words);
-    $revArrWords = array_reverse($arrWords);
-    $revWords = implode('', $revArrWords);
-    return $revWords;
-}
-
-print_r(getReverse('Привет мир!'));
+//function getReverse(string $words): string
+//{
+//    $arrWords = mb_str_split($words);
+//    $revArrWords = array_reverse($arrWords);
+//    $revWords = implode('', $revArrWords);
+//    return $revWords;
+//}
+//
+//print_r(getReverse('Привет мир!'));
 
 //Task 5
 
@@ -57,85 +53,115 @@ function isPrime(int $number): bool
     for ($i = 2; $i * $i <= $number; $i++) {
         if ($number % $i == 0) {
             return false;
-        } else {
-            return true;
         }
     }
     return true;
 }
 
-var_dump(isPrime(1));
+var_dump(isPrime(17));
 
 //Task 6
 
-function lenOfLastWord(string $words): mixed
-{
-    $arrOfWords = explode(" ", $words);
-    $countOfArr = count($arrOfWords);
-    if ($countOfArr > 1) {
-        return mb_strlen($arrOfWords[array_key_last($arrOfWords)]);
-    } else {
-        return 'Single word';
-    }
-}
-
-print_r(lenOfLastWord('hello, world'));
+//function lenOfLastWord(string $words): int
+//{
+//    $arrOfWords = explode(" ", $words);
+//    $lastWord = end($arrOfWords);
+//    $symbols = ['!', ',', ':', '.', ';'];
+//    $lastWord = str_replace($symbols, ' ', $lastWord);
+//    $countOfArr = count($arrOfWords);
+//    if ($countOfArr > 1) {
+//        return mb_strlen($lastWord);
+//    }
+//}
+//
+//print_r(lenOfLastWord('hello, world!'));
 
 //Task 7
 
-// я минус
-
-//Task 8
-
-function checkAnagram(string $firstWord, string $secondWord): bool
+function without(array $items, mixed $value) : array
 {
-    $arrOfFirst = mb_str_split($firstWord);
-    $countOfFirst = count($arrOfFirst);
-    $arrOfSec = mb_str_split($secondWord);
-    $countOfSec = count($arrOfSec);
-    $arrOfSame = [];
+    $filtered = array_filter($items, fn (mixed $item) => $item !== $value);
 
-    foreach ($arrOfFirst as $word) {
-        foreach ($arrOfSec as $value) {
-            if ($word === $value) {
-                $arrOfSame[] = $word;
-                break;
-            }
+    return array_values($filtered);
+}
+
+function combinations(array $arr): array
+{
+    if (count($arr) <= 1) {
+        return $arr;
+    }
+
+    $result = [];
+
+    foreach ($arr as $item) {
+        $combos = combinations(without($arr, $item));
+
+        foreach ($combos as $combo) {
+            $result[] = $item . $combo;
         }
     }
 
-    $stringOfSame = implode('', $arrOfSame);
-//    echo $stringOfSame;
-    if ($countOfFirst === $countOfSec and $stringOfSame === $firstWord) {
-        return true;
-    } else {
-        return false;
-    }
+    return $result;
 }
 
-var_dump(checkAnagram('спаниель', 'апельсин'));
+function getMax(array $arr): int
+{
+    return max(array_map('intval', combinations($arr)));
+}
+
+echo getMax([3, 24, 4]);
+
+//Task 8
+
+//function checkAnagram(string $firstWord, string $secondWord): bool
+//{
+//    $arrOfFirst = mb_str_split($firstWord);
+//    $countOfFirst = count($arrOfFirst);
+//    $arrOfSec = mb_str_split($secondWord);
+//    $countOfSec = count($arrOfSec);
+//    $arrOfSame = [];
+//
+//    foreach ($arrOfFirst as $word) {
+//        foreach ($arrOfSec as $value) {
+//            if ($word === $value) {
+//                $arrOfSame[] = $word;
+//                break;
+//            }
+//        }
+//    }
+//
+//    $stringOfSame = implode('', $arrOfSame);
+//
+//    return $countOfFirst === $countOfSec && $stringOfSame === $firstWord;
+//}
+//
+//var_dump(checkAnagram('спаниель', 'апельсин'));
 
 //Task 9
 
-function getTimesOf(int $bigNumber, int $number): int
-{
-    $bigNumber = (string)$bigNumber;
-    $arrOfNumbers = str_split($bigNumber);
-    $newArr = array_filter($arrOfNumbers, function ($item) use ($number) {
-        return $item === (string)$number;
-    });
-    return count($newArr);
-}
-
-print_r(getTimesOf(442158755745, 5));
+//function getTimesOf(int $bigNumber, int $number): int
+//{
+//    $bigNumber = (string)$bigNumber;
+//    $arrOfNumbers = str_split($bigNumber);
+//    $newArr = array_filter($arrOfNumbers, function ($item) use ($number) {
+//        return $item === (string)$number;
+//    });
+//    return count($newArr);
+//}
+//
+//print_r(getTimesOf(442158755745, 5));
 
 //Task 10
 
 function countOfWords(string $phrase): array
 {
     $phrase = strtolower($phrase);
+    $symbol = ['!', ',', ':', '.', ';'];
+    $symbolWithSpace = [' !', ' ,', ' :', ' .', ' ;'];
+    $phrase = str_replace($symbol, $symbolWithSpace, $phrase);
     $arrOfWords = explode(" ", $phrase);
+
     return array_count_values($arrOfWords);
 }
 
-print_r(countOfWords("That's the password: PASSWORD 123!")); // пока так, хз как разделить слова с знаками препинания
+print_r(countOfWords("That's the password: PASSWORD 123!"));
