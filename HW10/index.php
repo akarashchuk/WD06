2551638a95a9a76a87bf5292b5cf52d175597451
Kirill Goroshko <gorosheck@gmail.com>
2022-07-23 12:55:41 +0300

    hw 10 done

--- HW10/index.php
+++ HW10/index.php
@@ -1,41 +1,47 @@
 <?php
 
-$x = 'Hello';
-$y = 'World';
-echo ($x . ' ' . $y);
+// Task 1
+//$x = 'Hello World!';
 
+// Task 2
+//$x = 'Hello';
+//$y = 'World';
+//echo ($x . ' ' . $y);
 
-echo mb_strlen ('Привет мир!');
+// Task 3
+//echo mb_strlen ('Привет мир!');
 
-$oldText = "Learn HTML!";
-echo str_replace('HTML', "PHP", $oldText);
+// Task 4
+//$oldText = "Learn HTML!";
+//echo str_replace('HTML', "PHP", $oldText);
 
-$x = 50;
-$y = 10;
-if ($x > $y) {
-    echo 'x больше y';
-};
+// Task 5
+//$x = 50;
+//$y = 10;
+//if ($x > $y) {
+//    echo 'x больше y';
+//};
 
+// Task 6
+//$a = 20;
+//$b = 10;
+//if ($a === $b) {
+//    echo 'Первый';
+//} else if ($a > $b) {
+//    echo 'Второй';
+//} else {
+//    echo 'Третий';
+//};
 
-$a = 20;
-$b = 10;
-if ($a === $b) {
-    echo 'Первый';
-} else if ($a > $b) {
-    echo 'Второй';
-} else {
-    echo 'Третий';
-};
-
-
-$color = 'red';
-switch ($color) {
-    case 'red':
-        echo 'Hello';
-        break;
-    case 'green':
-        echo 'Второй';
-        break;
-    default:
-        echo 'Bye';
-}
+// Task 7
+//$color = 'red';
+//switch ($color) {
+//    case 'red':
+//        echo 'Hello';
+//        break;
+//    case 'green':
+//        echo 'Второй';
+//        break;
+//    default:
+//        echo 'Bye';
+//};
